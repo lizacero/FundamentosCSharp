562c0aab0b90f1c5a645b96231fd25a8da324502
Daniela Acero <danielalizethacero@gmail.com>
2025-03-19 15:52:51 -0500

    Ejercicio de Hola mundo en C#

---
+++ HolaMundo/HolaMundo/Form1.cs
@@ -0,0 +1,15 @@
+namespace HolaMundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void HolaMundo(object sender, EventArgs e)
+        {
+            MessageBox.Show("HOLA MUNDO");
+        }
+    }
+}
